97cdeba47abefc117ac7f0790d5966e1648beb53
Ab-Tayyab <abdullahtayyab414@gmail.com>
2026-04-02 09:32:24 +0500

    Day 2

---
+++ Fundamentals/conditionalStatements.java
@@ -0,0 +1,61 @@
+import java.util.*;
+public class conditionalStatements {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // even or odd
+
+        // System.out.println("enter number: ");
+        // int num = sc.nextInt();
+        // if(num%2==0){
+        //     System.out.println("Not an odd number");
+        // } 
+        // else{
+        //     System.out.println("Odd number");
+        // }
+
+        // calculator
+
+        // System.out.println("Enter First number");
+        // int fn = sc.nextInt();
+        // System.out.println("Enter Second number");
+        // int sn = sc.nextInt();
+
+        // String op = "*";
+        // if(op=="+"){
+        //     System.out.println("Addition is = "+ (fn+sn));
+        // }
+        // else if(op=="-"){
+        //     System.out.println("Subtraction is = "+ (fn-sn));
+        // }
+        // if(op=="*"){
+        //     System.out.println("Multiplication is = "+ (fn*sn));
+        // }
+        // if(op=="/"){
+        //     System.out.println("Division is = "+ (fn/sn));
+        // }
+        // if(op=="%"){
+        //     System.out.println("Modlus is = " + (fn%sn));
+        // }
+
+        // display month name 
+
+        System.out.println("Enter month number");
+        int num = sc.nextInt();
+        switch (num) {
+            case 1: System.out.println("January");break;
+            case 2: System.out.println("Fabuary");break;
+            case 3: System.out.println("March");break;
+            case 4: System.out.println("April");break;
+            case 5: System.out.println("May");break;
+            case 6: System.out.println("June");break;
+            case 7: System.out.println("July");break;
+            case 8: System.out.println("August");break;
+            case 9: System.out.println("September");break;
+            case 10: System.out.println("October");break;
+            case 11: System.out.println("November");break;
+            case 12: System.out.println("December");break;
+            default:System.out.println("Invalid Number");
+        }
+    }
+}
--- Fundamentals/variablesDataTypesInput.java
+++ Fundamentals/variablesDataTypesInput.java
@@ -2,11 +2,37 @@ import java.util.*;
 public class variablesDataTypesInput{
   public static void main (String args[]){
     Scanner sc = new Scanner(System.in);
-    System.out.println("Enter First number: ");
-    int a = sc.nextInt();
-    System.out.println("Enter Second Number: ");
-    int b = sc.nextInt();
-    int sum = a+b;
-    System.out.println("Sum is: " + sum) ;
+
+    // Sum of two numbers
+
+    // System.out.println("Enter First number: ");
+    // int a = sc.nextInt();
+    // System.out.println("Enter Second Number: ");
+    // int b = sc.nextInt();
+    // int sum = a+b;
+    // System.out.println("Sum is: " + sum) ;
+
+    // Calculate Radius and area 
+
+    System.out.println("Enter radius of a cicle");
+    int num = sc.nextInt();
+    double area = 3.14*(num*num);
+    double radius = Math.sqrt(num/3.14);
+    System.out.println("Area is = " + area + " Radius is = "+ radius);
+
+    // Print table
+
+    // System.out.println("Enter Table Number");
+    // int table = sc.nextInt();
+    // System.out.println(table + " * 1 = " + table*1 );
+    // System.out.println(table + " * 2 = " + table*2 );
+    // System.out.println(table + " * 3 = " + table*3 );
+    // System.out.println(table + " * 4 = " + table*4 );
+    // System.out.println(table + " * 5 = " + table*5 );
+    // System.out.println(table + " * 6 = " + table*6 );
+    // System.out.println(table + " * 7 = " + table*7 );
+    // System.out.println(table + " * 8 = " + table*8 );
+    // System.out.println(table + " * 9 = " + table*9 );
+    // System.out.println(table + " * 10 = " + table*10 );
   }
 }

a93b913fc81b17bbae9bda76ad23f6dedf7d5301
Ab-Tayyab <abdullahtayyab414@gmail.com>
2026-04-03 09:18:03 +0500

    Day 3

--- Fundamentals/conditionalStatements.java
+++ Fundamentals/conditionalStatements.java
@@ -1,6 +1,7 @@
 import java.util.*;
+
 public class conditionalStatements {
-    public static void main (String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
         // even or odd
@@ -8,54 +9,69 @@ public class conditionalStatements {
         // System.out.println("enter number: ");
         // int num = sc.nextInt();
         // if(num%2==0){
-        //     System.out.println("Not an odd number");
-        // } 
+        // System.out.println("Not an odd number");
+        // }
         // else{
-        //     System.out.println("Odd number");
+        // System.out.println("Odd number");
         // }
 
         // calculator
 
-        // System.out.println("Enter First number");
-        // int fn = sc.nextInt();
-        // System.out.println("Enter Second number");
-        // int sn = sc.nextInt();
+        System.out.println("Enter First number");
+        int fn = sc.nextInt();
+        System.out.println("Enter Second number");
+        int sn = sc.nextInt();
+        System.out.println("1 for +, 2 for -, 3 for *, 4 for /, 5 for %: ");
+        int op = sc.nextInt();
 
-        // String op = "*";
-        // if(op=="+"){
-        //     System.out.println("Addition is = "+ (fn+sn));
-        // }
-        // else if(op=="-"){
-        //     System.out.println("Subtraction is = "+ (fn-sn));
-        // }
-        // if(op=="*"){
-        //     System.out.println("Multiplication is = "+ (fn*sn));
-        // }
-        // if(op=="/"){
-        //     System.out.println("Division is = "+ (fn/sn));
-        // }
-        // if(op=="%"){
-        //     System.out.println("Modlus is = " + (fn%sn));
-        // }
+        switch (op) {
+            case 1:
+                System.out.println("Addition is = " + (fn + sn));
+                break;
+            case 2:
+                System.out.println("Subtraction is = " + (fn - sn));
+                break;
+            case 3:
+                System.out.println("Multiplication is = " + (fn * sn));
+                break;
+            case 4:
+                if (sn == 0) {
+                    System.out.println("Invalid division");
+                } else {
+                    System.out.println("Division is = " + (fn / sn));
+                    break;
+                }
+            case 5:
+                if (sn == 0) {
+                    System.out.println("Invalid division");
+                } else {
+                    System.out.println("Modlus is = " + (fn % sn));
+                    break;
+                }
+            default:
+                System.out.println("Invalid number!");
+                break;
 
-        // display month name 
-
-        System.out.println("Enter month number");
-        int num = sc.nextInt();
-        switch (num) {
-            case 1: System.out.println("January");break;
-            case 2: System.out.println("Fabuary");break;
-            case 3: System.out.println("March");break;
-            case 4: System.out.println("April");break;
-            case 5: System.out.println("May");break;
-            case 6: System.out.println("June");break;
-            case 7: System.out.println("July");break;
-            case 8: System.out.println("August");break;
-            case 9: System.out.println("September");break;
-            case 10: System.out.println("October");break;
-            case 11: System.out.println("November");break;
-            case 12: System.out.println("December");break;
-            default:System.out.println("Invalid Number");
         }
+
+        // display month name
+
+        // System.out.println("Enter month number");
+        // int num = sc.nextInt();
+        // switch (num) {
+        // case 1: System.out.println("January");break;
+        // case 2: System.out.println("Fabuary");break;
+        // case 3: System.out.println("March");break;
+        // case 4: System.out.println("April");break;
+        // case 5: System.out.println("May");break;
+        // case 6: System.out.println("June");break;
+        // case 7: System.out.println("July");break;
+        // case 8: System.out.println("August");break;
+        // case 9: System.out.println("September");break;
+        // case 10: System.out.println("October");break;
+        // case 11: System.out.println("November");break;
+        // case 12: System.out.println("December");break;
+        // default:System.out.println("Invalid Number");
+        // }
     }
 }
---
+++ Fundamentals/loops.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+public class loops {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // for loop
+
+        // for(int i=1; i<=10;i++){
+        // System.out.println(i);
+        // }
+
+        // do while loops
+
+        // int i=1;
+        // do{
+        // System.out.println(i);
+        // i++;
+        // }
+        // while(i<=10);
+
+        // while loop
+
+        // int j=1;
+        // while(j<=10){
+        // System.out.println(j);
+        // j++;
+        // }
+
+        // sum of first n natural number
+
+        // System.out.println("Enter last number: ");
+        // int n = sc.nextInt();
+        // int sum =0;
+        // for(int i=1;i<n;i++){
+        // sum +=i;
+        // System.out.println(sum);
+        // }
+        // System.out.println(sum);
+
+        // table of number
+
+        // System.out.println("Enter Table Number: ");
+        // int n = sc.nextInt();
+        // for(int i =1; i<=10;i++){
+        // System.out.println(n+" * "+i+" = "+n*i);
+        // }
+
+        // print even number
+
+        // System.out.println("Enter Last Number: ");
+        // int n = sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        // if(i%2==0){
+        // System.out.println(i);
+        // }
+        // }
+
+        // menu driven program
+
+        // int n = 1;
+        // do {
+        // System.out.println("Enter numbers out 100: ");
+        // int numbers = sc.nextInt();
+        // if (numbers >= 90) {
+        // System.out.println("This is good.");
+        // } else if (numbers >= 80) {
+        // System.out.println("This is also good");
+        // } else if (numbers >= 50) {
+        // System.out.println("Average.");
+        // } else {
+        // System.out.println("Fail");
+        // }
+        // System.out.println("Enter Number 1 or 0: ");
+        // n = sc.nextInt();
+        // } while (n == 1);
+
+        // prime number or not
+
+        System.out.println("Enter Number: ");
+        int n = sc.nextInt();
+        int count = 0;
+        if (n <= 1) {
+            System.out.println("Not a prime number.");
+            return;
+        }
+        for (int i = 2; i <=n/2; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        if (count ==0) {
+            System.out.println("Its a prime number.");
+        } else {
+            System.out.println("Not a prime number.");
+        }
+
+    }
+}

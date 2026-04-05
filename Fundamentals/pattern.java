5dd208cb5dde61739ee77b8d6a2de15d38b0deed
Ab-Tayyab <abdullahtayyab414@gmail.com>
2026-04-05 23:05:13 +0500

    Day 5

--- Fundamentals/pattern.java
+++ Fundamentals/pattern.java
@@ -1,55 +1,104 @@
 public class pattern {
-    public static void main(String args[]){
-        // solid rectangle 
+    public static void main(String args[]) {
+        // solid rectangle
 
         // for(int i=0;i<5;i++){
-        //     for(int j=0;j<=5;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
+        // for(int j=0;j<=5;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
         // }
 
-        // hollow rectangle 
+        // hollow rectangle
 
         // int n=5;
         // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         if(i==0 || i==n-1 || j==0 || j==n-1){
-        //             System.out.print("*");
-        //         }
-        //         else{
-        //             System.out.print(" ");
-        //         }
-        //     }
-        //     System.out.println();
+        // for(int j=0;j<n;j++){
+        // if(i==0 || i==n-1 || j==0 || j==n-1){
+        // System.out.print("*");
+        // }
+        // else{
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
         // }
 
-        // half pyramid 
+        // half pyramid
 
         // for(int i=0;i<5;i++){
-        //     for(int j=0;j<=i;j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
+        // for(int j=0;j<=i;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
         // }
 
-        // inverted half pyramid 
+        // inverted half pyramid
 
         // for(int i=0;i<5;i++){
-        //     for(int j=5-1;j>=i;j--){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
+        // for(int j=5-1;j>=i;j--){
+        // System.out.print("*");
+        // }
+        // System.out.println();
         // }
 
         // inverted half pyramid rotate by 180 deg
 
-        for(int i=0;i<5;i++){
-            for(int k=5-1;k>i;k--){
-                System.out.print(" ");
-            }
-            for(int j=0;j<=i;j++){
-                System.out.print("*");
+        // for(int i=0;i<5;i++){
+        // for(int k=5-1;k>i;k--){
+        // System.out.print(" ");
+        // }
+        // for(int j=0;j<=i;j++){
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // half pyramid with number
+
+        // for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // inverted half pyramid with number 
+
+        //  for (int i = 1; i <= 5; i++) {
+        //     for (int j = 5; j >= i; j--) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // floyd's triangle pattern 
+
+        // int num=1;
+        //  for (int i = 1; i <= 5; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         if(i==5){
+        //         System.out.print(num +" ");
+        //         }
+        //         else{
+        //             System.out.print(num +"  ");
+        //         }
+        //         num++;
+        //     }
+        //     System.out.println();
+        // }
+
+        // 0-1 pattern 
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                int sum = i+j;
+                if(sum%2==0){
+                    System.out.print(1 + " ");
+                }
+                else{
+                    System.out.print(0 + " ");
+                }
             }
             System.out.println();
         }

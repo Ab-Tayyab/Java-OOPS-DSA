09f0d0ed56344494be3d50a0e0dbf1147235d66c
Ab-Tayyab <abdullahtayyab414@gmail.com>
2026-04-18 23:51:47 +0500

    day 18

---
+++ Fundamentals/practice/dayThree.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class dayThree {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        // table of give number 
+
+        // System.out.print("Enter number: ");
+        // int table = sc.nextInt();
+        // for(int i=1;i<11;i++){
+        //     System.out.println(table + " X " + i+ " = "+table*i);
+        // }
+
+        // sum of all even number 
+
+        // System.out.println("Enter last digit: ");
+        // int num = sc.nextInt();
+        // int sum =0;
+        // for(int i=1;i<=num;i++){
+        //     if(i%2==0){
+        //         sum+=i;
+        //     }
+        // }
+        // System.out.println("Sum of 1 to "+num + " is: "+ sum );
+
+        // prime number from 1 to n 
+
+        System.out.println("Enter target number: ");
+        int prime = sc.nextInt();
+        boolean isPrime=true;
+        for(int i=2;i<=prime;i++){
+           
+        }
+    }
+}
